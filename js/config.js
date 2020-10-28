972ec86957bba6cba63796d05a2b2a91c4ae7c44
mgurkan27 <mustafagurkan27@gmail.com>
2020-10-27 21:36:36 -0400

    updated1

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-1_Bq5AhlE63
-        userPoolClientId: '', // e.g. 7mvpbko7e3bk8d8j5cpts1n82k
-        region: '' // e.g. us-east-1
+        userPoolId: 'us-east-1_Bq5AhlE63', // e.g. us-east-1_Bq5AhlE63
+        userPoolClientId: '7mvpbko7e3bk8d8j5cpts1n82k', // e.g. 7mvpbko7e3bk8d8j5cpts1n82k
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
