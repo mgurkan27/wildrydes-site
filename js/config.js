a5ddcfc2aa318a6b0c20c90b036627129b7492d5
mgurkan27 <mustafagurkan27@gmail.com>
2020-10-27 21:21:03 -0400

    updated1

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-1_Bq5AhlE63
+        userPoolClientId: '', // e.g. 7mvpbko7e3bk8d8j5cpts1n82k
         region: '' // e.g. us-east-2
     },
     api: {
