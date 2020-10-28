46e0ec3e29c04f1a547a35b756095d7aed008cae
mgurkan27 <mustafagurkan27@gmail.com>
2020-10-27 22:31:12 -0400

    updated1

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
+        invokeUrl: '' // e.g.  https://kqxi5eh7b5.execute-api.us-east-1.amazonaws.com/prod,
     }
 };
