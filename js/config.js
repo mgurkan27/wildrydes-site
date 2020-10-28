449214c4989d7a21e5254ec73810872bf40fe3e7
mgurkan27 <mustafagurkan27@gmail.com>
2020-10-27 21:32:06 -0400

    updated1

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: '', // e.g. us-east-1_Bq5AhlE63
         userPoolClientId: '', // e.g. 7mvpbko7e3bk8d8j5cpts1n82k
-        region: '' // e.g. us-east-2
+        region: '' // e.g. us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-east-1.amazonaws.com/prod',
     }
 };
